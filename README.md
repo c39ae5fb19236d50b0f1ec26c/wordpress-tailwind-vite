185e1d582d6cb01ab5b0d78995d098745c5232c2
Michał Koper <michal.koper@protonmail.com>
2023-06-22 16:21:16 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+## Theme requested for 'Wordpress Developer' job offer.
+
+Wordpress develepoment with Docker
+
+### Configuration
+
+Copy the example environment into `.env`
+
+```
+cp env.example .env
+```
+
+### Installation
+
+Open a terminal and `cd` to the folder in which `docker-compose.yml` is saved and run:
+
+```
+docker-compose up
+```
--- wp-app/wp-content/themes/mytheme/footer.php
+++ wp-app/wp-content/themes/mytheme/footer.php
@@ -8,16 +8,5 @@
   </footer>
 
 <?php wp_footer() ?>
-<!-- <script>
-    window.onscroll = function() {stickyNavbar()};
-var navbar = document.getElementById("navbar");
-function stickyNavbar() {
-    if (window.scrollY >= 100) {
-        navbar.classList.add("sticky", "top-0")
-    } else {
-        navbar.classList.remove("sticky", "top-0");
-    }
-}
-</script> -->
 </body>
 </html>
